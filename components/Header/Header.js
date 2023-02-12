881e87a522406ca53269ed60ec2343ec6f58c127
René Seib <70758072+reneseib@users.noreply.github.com>
2023-02-13 00:25:45 +0100

    fiddling

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -14,10 +14,16 @@ const Header = () => {
                         <li className={styles.navItem}>Mission</li>
                         <li className={styles.navItem}>CO<sub>2</sub> Kalkulator</li>
                         <li className={styles.navItem}>Über uns</li>
+                        <li className={styles.navItem}>Kontakt</li>
                     </ul>
                 </div>
+                <div className={styles.navIcons}>
+
+                    <svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiBox-root css-uqopch" focusable="false" aria-hidden="true" width="20" height="20" viewBox="0 0 24 24" data-testid="PersonOutlineIcon"><path d="M12 5.9c1.16 0 2.1.94 2.1 2.1s-.94 2.1-2.1 2.1S9.9 9.16 9.9 8s.94-2.1 2.1-2.1m0 9c2.97 0 6.1 1.46 6.1 2.1v1.1H5.9V17c0-.64 3.13-2.1 6.1-2.1M12 4C9.79 4 8 5.79 8 8s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 9c-2.67 0-8 1.34-8 4v3h16v-3c0-2.66-5.33-4-8-4z"></path></svg>
+
+                </div>
             </div>
-        </div>
+        </div >
     );
 };
 
